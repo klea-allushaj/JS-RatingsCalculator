9371591d2c3239c75d909a386e7cbda73a1f8a6a
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-07 18:24:37 +0000

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -3,5 +3,19 @@ const collect_ratings=()=>{
 
   let rating=0;
   const elements=document.querySelectorAll('.rating');
+  elements.forEach(element => {
+  rating=parseInt(element.id.replace('star', ''));
+  count +=parseInt(element.value);
+  sum +=parseInt(element.value)*rating;
 
+  });
+
+  if(rating.count!=0){
+    ratings.average=ratings.sum/ratings.count;
+  }
+  return ratings;
+};
+document.addEventListener('change',()=>{
+   const ratings = collect_ratings();
+   document.querySelector('#average').value = ratings.average.toFixed(2);
 });
